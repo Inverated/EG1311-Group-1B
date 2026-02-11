7b30d8a7511856333594a1d2e38dd5bbb8e32ff2
KQF <120025059+Inverated@users.noreply.github.com>
2026-02-11 11:59:25 +0800

    Update arduino code for new circuit layout

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -3,7 +3,7 @@
 /*
 Initialise:
 Set servo to initial angle
-Set sensor to low
+Set sensor and motor to low
 
 Start:
 Move forward until reached set sensor distance
@@ -15,24 +15,35 @@ End:
 After launch, floor reverse gear
 
 Untested:
-MotorPin1: Low; MotorPin2: Low => Stop
-MotorPin1: High; MotorPin2: High => Go
+MotorPin1: Low; MotorPin2: Low OR EnablePin: Low => Stop
+MotorPin1: High; MotorPin2: Low => Go
 MotorPin1: Low; MotorPin2: High => Rev
-MotorPin1: High; MotorPin2: Low => ?idk
+MotorPin1: High; MotorPin2: High => Not go (+ve & -ve same same level, no potential diff)
 */
 
-// Constants
-int TRIG_PIN = 13;
-int ECHO_PIN = 12;
-int MOTOR_PIN1 = 6;
-int MOTOR_PIN2 = 5;
+// Ultrasonic Sensor
+int TRIG_PIN = 12;
+int ECHO_PIN = 13;
 float SPEED_OF_SOUND = 0.0345;
 int STOPPING_DISTANCE = 10; 
 int STOP_TIME_TO_LAUNCH = 2000;
 
-// Servo Info
+// H-Bridge Motor Driver
+int MOTOR_SPEED = 255;  //0-255. Using constant speed for both motor to simplify things
+
+// Left Motor
+int LEFT_DRIVER_PIN1 = 2;
+int LEFT_DRIVER_PIN2 = 4;
+int LEFT_ENABLE_PIN = 5;
+
+// Right Motor
+int RIGHT_DRIVER_PIN1 = 7;
+int RIGHT_DRIVER_PIN2 = 8;
+int RIGHT_ENABLE_PIN = 6;
+
+// Servo Motor
 Servo sv;
-int SERVO_PIN = 9;
+int SERVO_PIN = 3;
 int INITIAL_ANGLE = 0;
 int LAUNCH_ANGLE = 180;
 
@@ -42,24 +53,40 @@ bool stopped = false;
 float since_stopped = 0;
 
 void setup() {
-  pinMode(MOTOR_PIN1, OUTPUT);
-  pinMode(MOTOR_PIN2, OUTPUT);
-
   pinMode(TRIG_PIN, OUTPUT);
-  digitalWrite(TRIG_PIN, LOW);
   pinMode(ECHO_PIN, INPUT);
+  digitalWrite(TRIG_PIN, LOW);  // Set receive to low to not trigger on start
+
+  pinMode(LEFT_DRIVER_PIN1, OUTPUT);
+  pinMode(LEFT_DRIVER_PIN2, OUTPUT);
+  pinMode(LEFT_ENABLE_PIN, OUTPUT);
+
+  pinMode(RIGHT_DRIVER_PIN1, OUTPUT);
+  pinMode(RIGHT_DRIVER_PIN2, OUTPUT);
+  pinMode(RIGHT_ENABLE_PIN, OUTPUT);
+
+  digitalWrite(LEFT_DRIVER_PIN1, LOW);
+  digitalWrite(LEFT_DRIVER_PIN2, LOW);
+  analogWrite(LEFT_ENABLE_PIN, MOTOR_SPEED);
+
+  digitalWrite(RIGHT_DRIVER_PIN1, LOW);
+  digitalWrite(RIGHT_DRIVER_PIN2, LOW);
+  analogWrite(RIGHT_ENABLE_PIN, MOTOR_SPEED);
 
   sv.attach(SERVO_PIN);
   sv.write(INITIAL_ANGLE);
-  Serial.begin(9600);
 
+  Serial.begin(9600);
 }
 
 void loop() {
   // Should reverse. Untested
   if (launched) {
-    digitalWrite(MOTOR_PIN1, LOW);
-    digitalWrite(MOTOR_PIN2, HIGH);
+    digitalWrite(LEFT_DRIVER_PIN1, LOW);
+    digitalWrite(LEFT_DRIVER_PIN2, HIGH);
+
+    digitalWrite(RIGHT_DRIVER_PIN1, LOW);
+    digitalWrite(RIGHT_DRIVER_PIN2, HIGH);
     return;
   }
 
@@ -75,8 +102,8 @@ void loop() {
   //Serial.print("Distance of: "); Serial.println(cms);
   if (cms <= STOPPING_DISTANCE) {
     // Stop the motor
-    digitalWrite(MOTOR_PIN1, LOW);
-    digitalWrite(MOTOR_PIN2, LOW);
+    analogWrite(LEFT_ENABLE_PIN, 0);
+    analogWrite(RIGHT_ENABLE_PIN, 0);
 
     if (!stopped) {
       since_stopped = millis();
@@ -95,8 +122,14 @@ void loop() {
   } else {
     stopped = false;
     since_stopped = 0;
-    digitalWrite(MOTOR_PIN1, HIGH);
-    digitalWrite(MOTOR_PIN2, HIGH);
+    analogWrite(LEFT_ENABLE_PIN, MOTOR_SPEED);
+    analogWrite(RIGHT_ENABLE_PIN, MOTOR_SPEED);
+
+    digitalWrite(LEFT_DRIVER_PIN1, HIGH);
+    digitalWrite(LEFT_DRIVER_PIN2, LOW);
+    
+    digitalWrite(RIGHT_DRIVER_PIN1, HIGH);
+    digitalWrite(RIGHT_DRIVER_PIN2, LOW);
   }
 
   // Delay required for ultra sonic sensor to read
