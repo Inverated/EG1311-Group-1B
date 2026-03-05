316e078fb001803a886992200bda39b1f7873abd
KQF <120025059+Inverated@users.noreply.github.com>
2026-03-05 13:29:47 +0800

    Update arduino code and schematics

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -32,20 +32,20 @@ int STOP_TIME_TO_LAUNCH = 1000;
 int MOTOR_SPEED = 255;  //0-255. Using constant speed for both motor to simplify things
 
 // Left Motor
-int LEFT_DRIVER_PIN1 = 2;
-int LEFT_DRIVER_PIN2 = 4;
+int LEFT_DRIVER_PIN1 = 4;
+int LEFT_DRIVER_PIN2 = 2;
 int LEFT_ENABLE_PIN = 5;
 
 // Right Motor
-int RIGHT_DRIVER_PIN1 = 7;
-int RIGHT_DRIVER_PIN2 = 8;
+int RIGHT_DRIVER_PIN1 = 8;
+int RIGHT_DRIVER_PIN2 = 7;
 int RIGHT_ENABLE_PIN = 6;
 
 // Servo Motor
 Servo sv;
 int SERVO_PIN = 3;
-int INITIAL_ANGLE = 0;
-int LAUNCH_ANGLE = 180;
+int INITIAL_ANGLE = 180;
+int LAUNCH_ANGLE = 90;
 
 // Others
 bool launched = false;
